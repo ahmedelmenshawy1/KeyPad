25bd0ea1f8017d3dd5da0d0bd383f17e2896273e
ahmed_elmenshawy <ahmed@10.145.3.108>
2016-04-06 22:05:13 +0200

    KeyPad

---
+++ KeyPad_Interface.h
@@ -0,0 +1,13 @@
+/*
+ * KeyPad_Interface.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: ahmed
+ */
+
+#ifndef KEYPAD_INTERFACE_H_
+#define KEYPAD_INTERFACE_H_
+
+void KeyPad_StructRead(void);
+
+#endif /* KEYPAD_INTERFACE_H_ */
